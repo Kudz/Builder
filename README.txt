eef9abaa480abcdb4e73695eabf651d98ad536a0
Kamil <kamilkuklinski@o2.pl>
2017-02-22 12:01:13 +0100

    modifing CMakeLists.txt README.txt, adding SDL2 windows dependencies

--- README.txt
+++ README.txt
@@ -1,2 +1,8 @@
-#On windows if you want to use MinGW compiler write:
-cmake -G "MinGW Makefiles"
+#On windows if you want to use CMake and MinGW compiler:
+1. Install MinGW.
+2. Install CMake.
+3. Open command line and go to folder where the respository is, then:
+mkdir build
+cd build
+cmake .. -G "MinGW Makefiles"
+mingw32-make
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+
+//#define SDL_MAIN_HANDLED
 #include <SDL.h>
+
+#include <cleanup.hpp>
 
 const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
@@ -9,24 +13,27 @@ int main(int argc, char *argv[])
 {
 
 	std::cout<<"begining"<<std::endl;
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
-		logSDLError(std::cout, "SDL_Init");
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cout<<"SDL_Init error"<<std::endl;
 		return 1;
 	}
 
 	std::cout<<"window"<<std::endl;
 	SDL_Window *window = SDL_CreateWindow("Shop builder", 100, 100, SCREEN_WIDTH,
 		SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	if (window == nullptr){
-		logSDLError(std::cout, "CreateWindow");
+	if (window == nullptr)
+	{
+		std::cout<<"CreateWindow error"<<std::endl;
 		SDL_Quit();
 		return 1;
 	}
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
 		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (renderer == nullptr){
-		logSDLError(std::cout, "CreateRenderer");
+	if (renderer == nullptr)
+	{
+		std::cout<<"CreateRenderer error"<<std::endl;
 		cleanup(window);
 		SDL_Quit();
 		return 1;
